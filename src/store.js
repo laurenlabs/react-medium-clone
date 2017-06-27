35c34b2304ca247c2189203514a89aeb4e952daf
lmariel <laurenmariel90@gmail.com>
2017-06-26 22:15:12 -0400

    commit from first night of exercises

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, createStore } from 'redux';
+import { promiseMiddleware } from './middleware';
+
+const defaultState = {
+  appName: 'Conduit',
+  articles: null
+};
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'HOME_PAGE_LOADED':
+    return { ...state, articles: action.payload.articles };
+  }
+  return state;
+};
+
+const middleware = applyMiddleware(promiseMiddleware);
+const store = createStore(reducer, middleware);
+
+export default store;
